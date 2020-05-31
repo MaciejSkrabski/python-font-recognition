a060031edd1df2e5fd7eca22d7e06500336254a2
MaciejSkrabski <maciej.skrabski@gmail.com>
2020-05-31 19:01:57 +0200

    test of csv

--- torch-learning.py
+++ torch-learning.py
@@ -4,6 +4,11 @@
 if __name__ == "__main__":
     from csv import reader
 
-    r = reader("dataset/git ")
+    l=[]
+    with open("dataset/dataset.csv") as csvfile:
+        r = reader(csvfile)
+        for row in r:
+            l.append(row)
+    print(l[:5], l[-5:], len(l))
 
 # %%
