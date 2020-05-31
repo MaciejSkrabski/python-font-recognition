e921d6a2d7bd0f717ea530ddbccf324c33d3e5da
MaciejSkrabski <maciej.skrabski@gmail.com>
2020-05-31 18:57:00 +0200

    dataset

--- torch-learning.py
+++ torch-learning.py
@@ -2,9 +2,8 @@
 
 #%%
 if __name__ == "__main__":
-    from dataset_creation import subset_of_csv, words_to_imgs
-    sub = subset_of_csv("words.csv", 600)
-    words_to_imgs(sub[:200], "fonty/Lato-Regular.ttf", "Lato-Regular", "testowe.csv")
-    words_to_imgs(sub[200:400], "fonty/LiberationSans-Regular.ttf", "LiberationSans-Regular", "testowe.csv")
-    words_to_imgs(sub[400:], "fonty/LiberationSerif-Regular.ttf", "LiberationSerif-Regular", "testowe.csv")
+    from csv import reader
+
+    r = reader("dataset/git ")
+
 # %%
