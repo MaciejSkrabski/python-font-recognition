52e71f31f5d8500eddd0d66178d0cc538a3531fe
MaciejSkrabski <maciej.skrabski@gmail.com>
2020-06-01 15:28:23 +0200

    from numpy to tensor, from tensor to numpy

--- dataset_creation.py
+++ dataset_creation.py
@@ -1,4 +1,6 @@
-#%%
+# %%
+
+
 def subset_of_csv(path, output_size):
     from csv import reader
     from random import sample
@@ -10,7 +12,9 @@ def subset_of_csv(path, output_size):
     new_word_list = sample(arr, k=output_size)
     return new_word_list
 
-#%%
+# %%
+
+
 def append_dict_as_row(file_name, dict_of_elem, field_names):
     from csv import DictWriter
     # Open file in append mode
@@ -20,70 +24,79 @@ def append_dict_as_row(file_name, dict_of_elem, field_names):
         # Add dictionary as wor in the csv
         dict_writer.writerow(dict_of_elem)
 # %%
-def words_to_imgs(wordlist=['raz', 'dwa', 'trzy'], font='', label='', csv_name='', W=300, H=80):
+
+
+def words_to_imgs(wordlist=['raz', 'dwa', 'trzy'], font='', label='',
+                  csv_name='', W=300, H=80):
     from random import randint
     from PIL import Image, ImageDraw, ImageFont
+    field_names = ['Word', 'Label']
 
-	
-    field_names = ['Word','Label']
-    
-    
     for word in wordlist:
         row_dict = {'Word': word, 'Label': label}
         append_dict_as_row(csv_name, row_dict, field_names)
 
-        img = Image.new('RGB', (W, H), color = "white")
-        fnt = ImageFont.truetype(font, randint(60,70))
+        img = Image.new('RGB', (W, H), color="white")
+        fnt = ImageFont.truetype(font, randint(60, 70))
         d = ImageDraw.Draw(img)
-        w,h = d.textsize(word, font=fnt)
+        w, h = d.textsize(word, font=fnt)
         d.text(((W-w)/2, (H-h)/2), word, fill="black", font=fnt)
-        img = img.rotate(randint(-2,2), resample = Image.BILINEAR, fillcolor="white", expand=0).resize([W, H])
+        img = img.rotate(
+            randint(-2, 2), resample=Image.BILINEAR,
+            fillcolor="white", expand=0).resize([W, H])
         img.save('dataset/'+word+'.jpg')
 # %%
+
+
 def shuffle_csv(input_file, output_file):
     from csv import writer, reader
     from random import shuffle
 
     results = []
-    
+
     # read csv to array
     with open(input_file) as csvfile:
         reader = reader(csvfile)
-        for row in reader: # each row is a list
+        for row in reader:  # each row is a list
             results.append(row)
 
     # shuffle
-    to_shuffle = results #skip row info
+    to_shuffle = results  # skip row info
     shuffle(to_shuffle)
 
     results = to_shuffle
 
-    #assign ids
+    # assign ids
     for idx, row in enumerate(results):
         row.insert(0, idx)
-    
-    # add "id" column
-    results.insert(0, ["id", "word", "label"]) 
 
-    #write 
+    # add "id" column
+    results.insert(0, ["id", "word", "label"])
+
+    # write
     with open('dataset/'+output_file, 'w', newline='') as csvfile:
         cwriter = writer(csvfile)
-        #for w in results:
-         #   cwriter.writerow(w)
+        # for w in results:
+        # cwriter.writerow(w)
         cwriter.writerows(results)
 # %%
-if __name__ == "__main__":
-    all_words_num=600
-    third=round((all_words_num/3.))
-    sub = subset_of_csv("words.csv", all_words_num)
-    lato=sub[:third]
-    sans=sub[third:(2*third)]
-    serif=sub[(2*third):3*third]
 
-    words_to_imgs(lato, "fonty/Lato-Regular.ttf", "Lato-Regular", "testowe.csv")
-    words_to_imgs(sans, "fonty/LiberationSans-Regular.ttf", "LiberationSans-Regular", "testowe.csv")
-    words_to_imgs(serif, "fonty/LiberationSerif-Regular.ttf", "LiberationSerif-Regular", "testowe.csv")
-    
+
+if __name__ == "__main__":
+    all_words_num = 600
+    third = round((all_words_num/3.))
+    sub = subset_of_csv("words.csv", all_words_num)
+    lato = sub[:third]
+    sans = sub[third:(2*third)]
+    serif = sub[(2*third):3*third]
+
+    words_to_imgs(lato, "fonty/Lato-Regular.ttf", "Lato-Regular",
+                  "testowe.csv")
+    words_to_imgs(sans, "fonty/LiberationSans-Regular.ttf",
+                  "LiberationSans-Regular", "testowe.csv")
+    words_to_imgs(serif, "fonty/LiberationSerif-Regular.ttf",
+                  "LiberationSerif-Regular", "testowe.csv")
+
     shuffle_csv('testowe.csv', 'dataset.csv')
 
 
--- torch-learning.py
+++ torch-learning.py
@@ -1,37 +1,43 @@
-#%%
+# %%
 from PIL import Image
 import numpy as np
 import torch
 import torchvision
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
+import sys
+from csv import reader
+
 
 print("CUDA available?", torch.cuda.is_available())
 
-class Img:
-    def __init__(self, row):
-        def to_numpy(word):
-            # loading image to numpy array
-            pic = np.asarray(Image.open("dataset/"+word+".jpg"))
-            pic = pic[:, :, 0]/255
 
-            
-            
-            
-            return pic # converting to float in range [0, 1]
-    
+class Img:
+    def to_tensor(self, numpyarr):
+        return torch.from_numpy(numpyarr)
+
+    def img_to_numpy(self, word):
+        # loading image to numpy array
+        pic = np.asarray(Image.open("dataset/"+word+".jpg"))
+        pic = pic[:, :, 0]/255
+        return pic  # converting to float in range [0, 1]
+
+    def to_numpy(self):
+        return self.data.numpy()
+
+    def __init__(self, row):
         # the object has id, a word depicted on it's image, a label
         # with the font used to write the word on the image and data,
         # which is a numpy array of pixels' brightnesses in range[0,1].
         self.id = int(row[0])
         self.word = row[1]
         self.label = row[2]
-        self.data = to_numpy(self.word)
-#%%
-from csv import reader
+        self.data = self.to_tensor(self.img_to_numpy(self.word))
 
-list_of_images=[]
-list_of_rows=[]
+
+# %%
+
+
+list_of_images = []
+list_of_rows = []
 with open("dataset/dataset.csv") as csvfile:
     r = reader(csvfile)
     next(r)
@@ -39,16 +45,24 @@ with open("dataset/dataset.csv") as csvfile:
         list_of_rows.append(row)
 
 # print(l[:5], l[-5:], len(l)) # test
-#%%
+# %%
+
+
 for row in list_of_rows[:3]:
     list_of_images.append(Img(row))
-#test
+# test
 row = list_of_images[0]
 row2 = list_of_images[2]
-print(row.id, row.word, row.label, row.data.shape)
-print(row2.id, row2.word, row2.label, row2.data.shape)
+print(row2.id, row2.word, row2.label, "\n", row2.data)
+
+print("\n\n", np.shape(row2.to_numpy()))
+
+# np.savetxt(sys.stdout, (row2.to_numpy())[:1], '%.001e')
 
 # for idx, val in enumerate(row.data):
 #     for inner, oy in enumerate(val):
 #         if oy!=1:
 #             print(idx, inner, oy)
+
+
+# %%
